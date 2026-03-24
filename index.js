336361e9be745d163067e540046f1c85d5e85b6e
Explosaodesabor <fla_mandy88@hotmail.com>
2026-03-24 16:31:32 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+console.log("Bot iniciado")
+
+function responder(msg){
+
+if(msg === "oi"){
+return `Olá 👋
+
+Sou o assistente virtual.
+
+1 - Cardápio
+2 - Promoção
+3 - Atendimento`
+}
+
+}
+
+console.log(responder("oi"))
